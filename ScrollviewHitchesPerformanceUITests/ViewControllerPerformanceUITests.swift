1ecbb297c3e32eaf263b5d87a19a774d96b1a33a
Yoshihisa Masaki <salem225@gmail.com>
2021-08-28 21:02:13 +0900

    add: ViewControllerPerformanceUITests

--- ScrollviewHitchesPerformanceUITests/ViewControllerPerformanceUITests.swift
+++ ScrollviewHitchesPerformanceUITests/ViewControllerPerformanceUITests.swift
@@ -7,7 +7,9 @@
 
 import XCTest
 
-class ViewControllerTestsUITests: XCTestCase {
+class ViewControllerPerformanceUITests: XCTestCase {
+    
+    private let app = XCUIApplication()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -25,9 +27,17 @@ class ViewControllerTestsUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCollectionViewScrollingPerformance() throws {
+        let collectionView = app.collectionViews.firstMatch
+        
+        let measureOptions = XCTMeasureOptions()
+        measureOptions.invocationOptions = [.manuallyStop]
+        
+        measure(metrics: [XCTOSSignpostMetric.scrollDecelerationMetric], options: measureOptions) {
+            collectionView.swipeUp(velocity: .fast)
+            stopMeasuring()
+            collectionView.swipeDown(velocity: .fast)
+        }
     }
 
 }
