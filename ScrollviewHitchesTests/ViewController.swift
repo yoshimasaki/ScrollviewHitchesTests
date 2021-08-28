f0c05e2740719cf322a899a84fc1d89b1f5b204c
Yoshihisa Masaki <salem225@gmail.com>
2021-08-28 20:20:34 +0900

    feat: implement extremely slow scroll item grid

--- ScrollviewHitchesTests/ViewController.swift
+++ ScrollviewHitchesTests/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        // simulate extremely slow cell content configuration and sluggish scroll
+        Thread.sleep(forTimeInterval: 0.05)
+        
+        cell.contentView.backgroundColor = .random
+        
+        return cell
+    }
+}
+
+extension UIColor {
+    
+    static var random: UIColor {
+        UIColor(red: .random(in: .zero..<1.0), green: .random(in: .zero..<1.0), blue: .random(in: .zero..<1.0), alpha: 1.0)
+    }
+}
